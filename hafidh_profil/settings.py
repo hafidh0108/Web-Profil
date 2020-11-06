6b9281027640f4bb817a4d982cfad50d8fa81cd0
hafidh0108 <hafidh.difa@gmail.com>
2020-11-06 19:17:08 +0700

    setting database

--- hafidh_profil/settings.py
+++ hafidh_profil/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'hafidh_profil.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.web_profil',
     }
 }
 
